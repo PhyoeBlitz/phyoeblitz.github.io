99f063ecd3fdf4e3ba0f3a2364ee6c5b2fbcea42
Phyoe <39774502+PhyoeBlitz@users.noreply.github.com>
2025-03-04 07:58:22 +0000

    update job info

--- info.js
+++ info.js
@@ -9,7 +9,9 @@ window.p_2_content = "I passed the Japanese Language Proficiency Test N3 in Marc
 //jobs
 window.jobsArray = [
     "axrossroad株式会社",
-    "10/2023-now",
+    "10/2023-12/2024",
+    "株式会社YTT Links",
+    "01/2025-now",
     "I'm working as a Software Engineer in Tokyo."
 ];
 
